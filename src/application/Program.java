de1728702897a1663fc79cd3c151c16200bc1c99
Helio Haruo <haruosugano@icloud.com>
2021-01-27 09:51:21 -0300

    Handling exceptions and clearing screen

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,9 @@
 package application;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import chess.ChessException;
 import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
@@ -14,16 +16,28 @@ public class Program {
 		ChessMatch chessMatch = new ChessMatch();
 		
 		while (true) {
-			UI.printBoard(chessMatch.getPiece());
-			System.out.println();
-			System.out.print("Source: ");
-			ChessPosition source = UI.readChessPosition(sc);
-			
-			System.out.println();
-			System.out.print("Target: ");
-			ChessPosition target = UI.readChessPosition(sc);
-			
-			ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+			try {
+				UI.clearScreen();/*Tem a funcao de limpar a tela, so mostra a tela do xadrez atualizada*/
+				UI.printBoard(chessMatch.getPiece());
+				System.out.println();
+				System.out.print("Source: ");
+				ChessPosition source = UI.readChessPosition(sc);
+				
+				System.out.println();
+				System.out.print("Target: ");
+				ChessPosition target = UI.readChessPosition(sc);
+				
+				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+			}
+			catch(ChessException e){
+				System.out.println(e.getMessage());
+				sc.nextLine();/*Excessao para que o usuario, retorne a inserir o dados*/
+			}
+			catch(InputMismatchException e){
+				System.out.println(e.getMessage());
+				sc.nextLine();/*Excessao para que o usuario, retorne a inserir o dados*/
+			}
+
 		}
 	}
 
